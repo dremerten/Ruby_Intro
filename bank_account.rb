a392b298ae422a2505af35d2b8a5dcf2a0813e47
dremerten <dremerten@users.noreply.github.com>
2019-02-02 15:32:48 -0800

    Update bank_account.rb file
    Added all code comments to explain code clearly

--- bank_account.rb
+++ bank_account.rb
@@ -1,11 +1,16 @@
+
+# create BankAccount Class
 class BankAccount
+  # allow attribute reader, to be read only
   attr_reader :name
 
+  # define initialize, with customer name and empty array
   def initialize(name)
     @name = name
     @transactions = []
   end
 
+  # add methods to push data to transactions array, both debit and credit the account balance
   def add_transactions(description, amount)
     @transactions.push("description": description, "amount": amount)
   end
@@ -18,6 +23,7 @@ class BankAccount
     add_transactions(description, -amount)
   end
 
+  # define the account balance with starting value of 0.00
   def balance
     balance = 0.00
     @transactions.each do | transaction|
@@ -26,10 +32,12 @@ class BankAccount
     return balance
   end
 
+  # define 'to string' method that prints useful account information
   def to_s
     "Name: #{name}, Balance: #{sprintf("%0.2f", balance)}"
   end
 
+  # Print the registered account holder's name
   def print_register
     puts "#{name}'s' Bank Account"
     puts "/" * 40
@@ -50,7 +58,7 @@ class BankAccount
 
 
 end
-
+# These are just used for Testing (lines 62-68)
 bank_account = BankAccount.new("Archie")
 bank_account.credit("Paycheck", 100)
 bank_account.debit("Groceries", 40)
